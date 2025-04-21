f7d7e9fecd09df859875c6bbee8c69f593ff656a
Rava <rava.ghiffari@gmail.com>
2025-04-21 08:46:40 +0700

    Changing Design Interface

--- interface.py
+++ interface.py
@@ -11,22 +11,41 @@ def main():
     pipeline = jb.load('dataset_B_pipeline.joblib')
     preprocessor = pipeline['preprocessor']
     model = pipeline['best_model'] #RF
-    feature_names = pipeline['feature_names']
 
-    data = pd.read_csv('Dataset_B_hotel.csv')
+    # Initialize session state
+    if 'reset_form' not in st.session_state:
+        st.session_state.reset_form = False
+        st.session_state.prediction_done = False
 
-    with st.expander("Data overview"):
-        st.write("### Data Primer")
-        st.write(f"Terdapat {data.shape[0]} data")
-        st.dataframe(data[:])
-        st.write("Some features like `booking_id`, `arrival date` wouldn't be needed")
+    # Reset function
+    def reset_form():
+        st.session_state.update({
+            'adults': 2,
+            'child': 0,
+            'weekend_nights': 1,
+            'week_nights': 2,
+            'meal_type': 'Meal Plan 1',
+            'req_park': 0,
+            'room_type': 'Room Type 1',
+            'lead_time': 14,
+            'arrival_year': 2018,
+            'arrival_month': 6,
+            'market_seg': 'Online',
+            'repeated_guest': 0,
+            'prev_cancel': 0,
+            'prev_books_not_cancel': 0,
+            'avg_price_room': 85.0,
+            'special_req': 1,
+            'reset_form': True,
+            'prediction_done': False
+        })
 
     st.write("## Masukkan Details dari tipe pemesanan yang ingin dioperasikan")
     tc1, tc2 = st.columns(2)
     s = st.session_state
 
     with tc1:
-        if st.button("🔴 Test Case 1 (Cancelled)"):
+        if st.button("🔴 High Cancellation Risk", help="Menunjukkan skema booking yang kemungkinan gagal(Canceled)"):
             s.adults = 1
             s.child = 0
             s.weekend_nights = 1
@@ -45,7 +64,7 @@ def main():
             s.special_req = 0
     
     with tc2:
-        if st.button("🟢 Test Case 2 (Not_Cancelled)"):
+        if st.button("🟢 Low Cancellation Risk", help="Menunjukkan skema booking yang mungkin sukses(Not Canceled)"):
             s.adults = 2
             s.child = 0
             s.weekend_nights = 1
@@ -64,24 +83,36 @@ def main():
             s.special_req = 1
 
     # input variables
-    adults = st.number_input("Adult Total",1,3, key="adults")
-    child = st.number_input("Child Total", 0,2, key="child")
-    weekend_nights = st.slider("Weekend Nights", 0, 7, key="weekend_nights")
-    week_nights = st.slider("Week Nights", 0,17, key="week_nights")
-    meal_type = st.selectbox("Meal plan type", ['Meal Plan 1', 'Meal Plan 2', 'Meal Plan 3', 'Not Selected'], key="meal_type")
-    req_park = st.selectbox("Required parking space", [0,1], key="req_park")
-    room_type = st.selectbox("Room type",['Room Type 1', 'Room Type 2', 'Room Type 3', 'Room Type 4', 'Room Type 5', 'Room Type 6', 'Room Type 7'], key="room_type")
-    lead_time = st.number_input('Lead time', 0,443, key="lead_time")
-    arrival_year = st.selectbox('Arrival year', [2017,2018], key="arrival_year")
-    arrival_month = st.number_input("Arrival month", 1,12,key="arrival_month")
-    market_seg = st.selectbox('Market Segment Type', ['Aviation', 'Complementary', 'Corporate', 'Offline', 'Online'], key="market_seg")
-    repeated_guest = st.selectbox("Repeated Guest", [0,1], key="repeated_guest")
-    prev_cancel = st.number_input('Previous Cancellations', 0,13, key = "prev_cancel")
-    prev_books_not_cancel = st.number_input('Previouse bookings Not Cancelled', 0,58, key="prev_books_not_cancel")
-    avg_price_room = st.number_input('Average Price per Room', 0.0,540.0, key="avg_price_room")
-    special_req = st.number_input('Number of Special Requests',0,5, key="special_req")
-    
-    user_input = pd.DataFrame([{
+    st.write("## 📝 Detail Pemesanan")
+
+    with st.form("booking_form"):
+        col1, col2 = st.columns(2)
+
+        with col1:
+
+            adults = st.number_input("Adult Total",1,3, key="adults")
+            child = st.number_input("Child Total", 0,2, key="child")
+            weekend_nights = st.slider("Weekend Nights", 0, 7, key="weekend_nights")
+            week_nights = st.slider("Week Nights", 0,17, key="week_nights")
+            meal_type = st.selectbox("Meal plan type", ['Meal Plan 1', 'Meal Plan 2', 'Meal Plan 3', 'Not Selected'], key="meal_type")
+            req_park = st.selectbox("Required parking space", [0,1], key="req_park")
+            room_type = st.selectbox("Room type",['Room Type 1', 'Room Type 2', 'Room Type 3', 'Room Type 4', 'Room Type 5', 'Room Type 6', 'Room Type 7'], key="room_type")
+        with col2:
+
+            lead_time = st.number_input('Lead time', 0,443, key="lead_time")
+            arrival_year = st.selectbox('Arrival year', [2017,2018], key="arrival_year")
+            arrival_month = st.number_input("Arrival month", 1,12,key="arrival_month")
+            market_seg = st.selectbox('Market Segment Type', ['Aviation', 'Complementary', 'Corporate', 'Offline', 'Online'], key="market_seg")
+            repeated_guest = st.selectbox("Repeated Guest", [0,1], key="repeated_guest")
+            prev_cancel = st.number_input('Previous Cancellations', 0,13, key = "prev_cancel")
+            prev_books_not_cancel = st.number_input('Previouse bookings Not Cancelled', 0,58, key="prev_books_not_cancel")
+            avg_price_room = st.number_input('Average Price per Room', 0.0,540.0, key="avg_price_room")
+            special_req = st.number_input('Number of Special Requests',0,5, key="special_req")
+
+        submitted = st.form_submit_button("🔍 Predict Booking Status")
+
+    if submitted:
+        user_input = pd.DataFrame([{
             'no_of_adults' : adults,
             'no_of_children': child,
             'no_of_weekend_nights' : weekend_nights,
@@ -99,26 +130,24 @@ def main():
             'avg_price_per_room':avg_price_room,
             'no_of_special_requests':special_req
         }])
-    with st.expander("Data yang digunakan"):
+        #Button
         st.dataframe(user_input)
-    
-    #Button
-    if st.button('Classify'):
         try:
             X_processed = preprocessor.transform(user_input)
         
-        # prediction and probabilities
+            # prediction and probabilities
             pred = model.predict(X_processed)[0]
             pred_proba = model.predict_proba(X_processed)[0]
         
             if 'label_encoder' in pipeline:
+                pred_label = "Canceled" if pred == 0 else "Not Canceled"
+                classes = ["Not Canceled", "Canceled"]
+            else:           
                 pred_label = pipeline['label_encoder'].inverse_transform([pred])[0]
                 classes = pipeline['label_encoder'].classes_
-                 
-            else:           
-                pred_label = "Cancelled" if pred == 1 else "Not Cancelled"
-                classes = ["Not Canceled", "Canceled"]
-        
+
+            st.success("🎯 Prediction Complete!")
+
         # hasil secara spesifik
             st.write("### Prediction Results:")
             col1, col2 = st.columns(2)
@@ -139,9 +168,15 @@ def main():
         
         # event yang mungkin dari prediksi
             if pred == 1:  # Not Cancelled
-                st.success("This booking is likely to be honored")
+                st.success("✅ This booking is likely to be honored")
             else:
-                st.error("This booking has high cancellation risk")
+                st.error("⚠️ This booking has high cancellation risk")
+                st.write("#### Prediction details:")
+                st.write("Faktor Penyebab:")
+                if lead_time > 100:
+                    st.write(f"- Lead Time Tinggi (**{lead_time} hari**)")
+                if prev_cancel > 0:
+                    st.write(f"- Riwayat Cancel (**{prev_cancel}x**)")
             
         except Exception as e:
             st.error(f"Error during prediction: {str(e)}")    
